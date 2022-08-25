086807e9408e6857fb1c58a68902118e36b34339
Dharmik Anghan <108571957+dharmik-anghan@users.noreply.github.com>
2022-08-26 00:27:26 +0530

    Update 8.c

--- Assignment-12/8.c
+++ Assignment-12/8.c
@@ -1,32 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int binary(int);
-
+void dtob(int);
 int main()
 {
     int n;
-    printf("NUMBER: ");
+    printf("VALUE: ");
     scanf("%d", &n);
 
-    binary(n);
+    dtob(n);
 
     return 0;
 }
 
-int binary(int n)
+void dtob(int n)
 {
-    int s;
-    if(n==0)
-        return 1;
-    if(n%2 != 0)
-        s=1;
-    else
-        s=0;
-    binary(n%2);
-    
-    
-    printf("%d ", s);
-
-    return s;
-
+    if(n>0)
+    {
+        dtob(n/2);
+        printf("%d", n%2);
+    }
+            
 }
