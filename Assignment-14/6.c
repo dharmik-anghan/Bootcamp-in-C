cf76c180a4c4bcb3163bca8fb23fa2f73a04e96b
Dharmik Anghan <108571957+dharmik-anghan@users.noreply.github.com>
2022-08-26 12:46:14 +0530

    Add files via upload

---
+++ Assignment-14/6.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int sort[10], temp;
+    printf("Enter 10 number: \n");
+
+    for(int i = 0; i < 10; i++)
+        scanf("%d", &sort[i]);
+    
+    for(int i = 0; i <9; i++)
+    {
+        for(int j=i+1; j < 10; j++)
+        {
+            if(sort[i]>sort[j])
+            {
+                temp = sort[i];
+                sort[i] = sort[j];
+                sort[j] = temp;
+            }
+        }
+    }
+    for(int i = 0; i < 10; i++)
+    {
+        printf("%d ", sort[i]);
+    }
+    return 0;
+}
