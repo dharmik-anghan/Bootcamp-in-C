6f3a19e792ff0b26419ed4621e4b17e57df37104
Dharmik Anghan <108571957+dharmik-anghan@users.noreply.github.com>
2022-09-01 16:35:11 +0530

    Add files via upload

---
+++ Assignment-18/9.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+void swap(char a[], int i, int j)
+{
+    char temp;
+
+    while(i<=j) 
+    {
+        temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+        i++;
+        j--;
+    }    
+}
+int main()
+{
+    char a[20] = "My  name is Dharmik";
+
+    int i=0,start=0,end=0,flag=0;
+    while(a[i] != '\0')
+    {
+        while(a[i] != ' ')
+        {
+            if(a[i] == '\0')
+            {
+                flag=1;
+                break;
+            }
+            end++;
+            i++;
+        }
+        swap(a, start, end-1);
+        if(flag==1)
+            break;
+        start=end++;
+        i++;
+    }
+    swap(a,0,i-1);
+    printf("%s", a);
+    return 0;
+
+}
