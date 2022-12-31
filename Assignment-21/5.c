dd59edc94318fb833350f525ac11a9c186e4e56f
Dharmik Anghan <108571957+dharmik-anghan@users.noreply.github.com>
2022-12-31 15:06:43 +0530

    Add files via upload

---
+++ Assignment-21/5.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <string.h>
+
+struct employee 
+{
+    int id;
+    char name[50];
+    float salary;
+}e[5];
+
+struct employee input()
+{
+    struct employee b;
+    printf("Enter ID, Name and Salary: ");
+    scanf("%d", &b.id);
+    fflush(stdin);
+    fgets(b.name, 50, stdin);
+    b.name[strlen(b.name)-1] = '\0';
+    scanf("%f", &b.salary);
+
+    return b;
+}
+
+void display(struct employee b)
+{
+    printf("\nID     = %d\nName   = %s\nSalary = %2.f\n", b.id,b.name,b.salary);
+}
+
+void sortbyprice(struct employee e[], int size)
+{
+    int i, j;
+    struct employee temp;
+
+    for(int i = 0; i < size-1; i++)
+    {
+        for(int j=i; j < size; j++)
+        {
+            if(e[i].salary < e[j].salary)
+            {
+                temp = e[i];
+                e[i] = e[j];
+                e[j] = temp;
+
+            }
+        }
+    }
+}
+
+int main()
+{
+    for(int i =0; i < 5; i++)
+    {
+        e[i] = input();
+    }
+
+    sortbyprice(e,5);
+
+    for(int i=0; i < 5; i++)
+        display(e[i]);
+    
+    return 0;
+}
