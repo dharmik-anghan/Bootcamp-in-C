10b239f8397c3fcdbf570de68dbe66cb83a8364c
Dharmik Anghan <108571957+dharmik-anghan@users.noreply.github.com>
2023-01-06 22:58:39 +0530

    Add files via upload

---
+++ Assignment-23/7.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, sq;
+
+    cout<<"Enter number"<<endl;
+    cin>>a;
+
+    sq = a*a;
+
+    cout<< "sq = " << sq <<endl;
+
+
+    return 0;
+}
