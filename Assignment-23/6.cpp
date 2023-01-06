c940ed0c85868ef6c3e8b69a19fa12c7a9a4ccd5
Dharmik Anghan <108571957+dharmik-anghan@users.noreply.github.com>
2023-01-06 22:58:23 +0530

    Add files via upload

---
+++ Assignment-23/6.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, sum;
+
+    cout<<"Enter 3 Number to sum"<<endl;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+
+    sum = a+b+c;
+
+    cout<< "sum = " << sum <<endl;
+
+
+    return 0;
+}
