e7e0c6b17b54c3caae6c44193f9f58c0dece42fc
Dharmik Anghan <108571957+dharmik-anghan@users.noreply.github.com>
2023-01-10 18:22:16 +0530

    Add files via upload

---
+++ Assignment-22/6.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int *ptr, n;
+    printf("How many number you wanna enter?\n");
+    scanf("%d", &n);
+
+    ptr = (int*)malloc(n*sizeof(int));
+    if(ptr == NULL)
+    {
+        printf("Memory Allocation Failded");
+        return 0;
+    }
+
+    for(int i=0; i < n; i++)
+    {
+        scanf("%d", ptr+i);
+    }
+
+    for(int i=0; i < n-1; i++)
+    {
+        for(int j=i; j < n; j++)
+        {
+            if(ptr[i] <= ptr[j])
+            {
+                int t = ptr[i];
+                ptr[i] = ptr[j];
+                ptr[j] = t;
+            }
+        }
+    }
+
+    printf("\n\n%d", *(ptr+0));
+    free(ptr);
+    
+
+    return 0;
+}
