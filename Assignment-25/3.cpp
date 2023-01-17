12d5c778a6ca12bcc55dc97f67bb7a1828f589ff
Dharmik Anghan <108571957+dharmik-anghan@users.noreply.github.com>
2023-01-17 10:30:43 +0530

    Add files via upload

--- Assignment-25/3.cpp
+++ Assignment-25/3.cpp
@@ -1,24 +1,50 @@
 #include <iostream>
 using namespace std;
-void FindPower(int, int);
+
+class Factorial
+{
+    private:
+        int n;
+        int fact;
+    public:
+        void setN(int x)
+        {
+            n = x;
+        }
+
+        int getN()
+        {
+            return n;
+        }
+
+        int getFactorial()
+        {
+            return fact;
+        }
+
+        void calculatefactorial()
+        {
+            int f=1;
+            if(n <= 0)
+            {
+                fact = 1;
+            }
+            for(int i = 1; i <= n; i++)
+            {
+                f *= i;
+            }
+            fact = f;
+        }
+};
 
 int main()
 {
-    int x = 2, y=3;
-    FindPower(x,y);
+    Factorial f1, f2;
+
+    f1.setN(5);
+
+    f1.calculatefactorial();
+    cout<<"Factorial of "<<f1.getN()<<" is "<<f1.getFactorial()<<endl;
 
     return 0;
-}
-
-void FindPower(int a, int b)
-{
-    int answer=1;
-
-    for(int i=1; i <= b; i++ )
-    {
-        answer *= a;
-    }
-
-    cout<<answer<<endl;
-    
 }
