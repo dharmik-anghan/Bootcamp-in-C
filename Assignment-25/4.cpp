cc0805b78ff6d8667f53a0d504bb365b8eafb332
Dharmik Anghan <108571957+dharmik-anghan@users.noreply.github.com>
2023-01-17 10:30:59 +0530

    Add files via upload

--- Assignment-25/4.cpp
+++ Assignment-25/4.cpp
@@ -1,39 +1,62 @@
 #include <iostream>
 using namespace std;
 
-int fact(int x)
+class LargestNumber
 {
-    if (x <= 0)
-        return 1;
-    int fact = 1;
-    for (int i = 1; i <= x; i++)
-        fact = fact * i;
+    private:
+        int a, b, c, large;
+    public:
+        void setValue(int x, int y, int z)
+        {
+            a = x;
+            b = y; 
+            c = z;
+        }
 
-    return fact;
-}
+        int getA()
+        {
+            return a;
+        }
+        int getB()
+        {
+            return b;
+        }
+        int getC()
+        {
+            return c;
+        }
 
-int comb(int n, int r)
-{
-    int com = fact(n) / (fact(r) * fact(n - r));
-    return com;
-}
+        int getLargeValue()
+        {
+            return large;
+        }
+
+        void calculateLValue()
+        {
+            if(a >= b && a>=c)
+                large = a;
+            else if(b >= c)
+                large = b;
+            else    
+                large = c;
+        }
+};
 
 int main()
 {
-    int n;
-    cout << "Enter Number";
-    cin >> n;
+    LargestNumber l1, l2, l3;
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j <= i; j++)
-        {
-            int k = 0;
-            if (j == 0)
-                while (k++ <= (n / 2 - i + 1))
-                    cout << " ";
-            printf("%d ", comb(i, j));
-        }
-        cout << endl;
-    }
+    l1.setValue(45,34,21);
+    l2.setValue(23,43,5);
+    l3.setValue(23,45,76);
+
+    l1.calculateLValue();
+    l2.calculateLValue();
+    l3.calculateLValue();
+
+    cout<<"Largest value among "<<l1.getA()<<", "<<l1.getB()<<", "<< l1.getC()<< " is "<<l1.getLargeValue()<<endl;
+    cout<<"Largest value among "<<l2.getA()<<", "<<l2.getB()<<", "<< l2.getC()<< " is "<<l2.getLargeValue()<<endl;
+    cout<<"Largest value among "<<l3.getA()<<", "<<l3.getB()<<", "<< l3.getC()<< " is "<<l3.getLargeValue()<<endl;
+
+    return 0;
 }
