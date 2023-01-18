ca1128a0dbf38c9ecdce7decf1dddf324eee64cc
Dharmik Anghan <108571957+dharmik-anghan@users.noreply.github.com>
2023-01-18 22:46:26 +0530

    Add files via upload

---
+++ Assignment-26/4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Counter
+{
+    public:
+        Counter()
+        {
+            cout<<"Counter"<<endl;
+        }
+};
+int main()
+{
+    Counter c1, c2, c3, c4;
+
+    return 0;
+}
