13f9ffdb4f74575c74fc978330ce052f12db4dac
Dharmik Anghan <108571957+dharmik-anghan@users.noreply.github.com>
2023-01-25 14:03:08 +0530

    Create 1.cpp

---
+++ Assignment-27/1.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+class Complex
+{
+    private:
+        int real;
+        int img;
+    public:
+        Complex()
+        {
+            
+        } 
+        Complex(int r, int i)
+        {
+            real = r;
+            img = i;
+        }
+        Complex operator+(Complex C)
+        {
+            Complex temp;
+            temp.real = real + C.real;
+            temp.img = img + C.img;
+
+            return temp;
+        }
+        Complex operator-(Complex C)
+        {
+            Complex temp;
+            temp.real = real - C.real;
+            temp.img = img - C.img;
+
+            return temp;
+        }
+        Complex operator*(Complex C)
+        {
+            Complex temp;
+            temp.real = real * C.real;
+            temp.img = img * C.img;
+
+            return temp;
+        }
+        int operator==(Complex C)
+        {
+            if(real == C.real && img == C.img)
+                return true;
+            else
+                return false;
+        }
+        void display()
+        {
+            cout<<"Real = "<<real<<" Img = "<<img<<endl;
+        }                                                                                                                                                                                    
+};
+
+int main()
+{
+    Complex c1(3,5);
+    Complex c2(3,5);
+    Complex c3,c4,c5;
+
+    c3 = c1 + c2;
+    c4 = c1 - c2;
+    c5 = c1 * c2;
+
+    c1.display();
+    c2.display();
+    c3.display();
+    c4.display();
+    c5.display();
+
+    if(c1 == c2)
+        cout<<"Equal"<<endl;
+    else
+        cout<<"Not Equal"<<endl;
+
+    return 0;
+}
